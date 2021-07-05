dce3df4291b9e557395266bc6f34056ff8612aba
Christian Bosse <christianbosse1998@gmail.com>
2021-07-05 12:17:08 -0300

    Added some changes and text

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,6 +1,7 @@
 import React from "react";
 import landingVideo from "../Media/landingpage.mp4";
 import styled from "styled-components";
+import Typist from "react-typist";
 
 const Welcome = () => {
   return (
@@ -8,6 +9,42 @@ const Welcome = () => {
       <Video autoPlay loop muted>
         <source src={landingVideo} type="video/mp4" />
       </Video>
+
+      <Typist cursor={{ hideWhenDone: true }}>
+        Hello, my name is Christian Bosse.
+        <Typist.Delay ms={1000} />
+        <br />
+        Welcome to my amaz
+        <Typist.Delay ms={0} />
+        nig
+        <Typist.Backspace count={3} delay={800} />
+        <Typist.Delay ms={600} />
+        ing Website
+        <Typist.Delay ms={1000} />
+        <br />
+      </Typist>
+
+      <Typist className="TypistExample-message" cursor={{ hideWhenDone: true }}>
+        * Easy to style
+        <Typist.Delay ms={1250} />
+        <br />
+        * Easy to customize
+        <Typist.Delay ms={1250} />
+        <br />
+        * Easy to use backp
+        <Typist.Delay ms={500} />
+        sace
+        <Typist.Backspace count={5} delay={1000} />
+        <Typist.Delay ms={750} />
+        space
+        <Typist.Delay ms={1250} />
+        <br />
+        <span>
+          * <span className="flash">docs</span>
+        </span>
+        <br />
+        {""}
+      </Typist>
     </>
   );
 };
