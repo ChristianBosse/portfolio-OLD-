c2dac852f06c071c1094cd92d4b2c27a30eb1431
Christian Bosse <christianbosse1998@gmail.com>
2021-07-05 14:58:30 -0300

    fixes and framework done.

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -2,49 +2,44 @@ import React from "react";
 import landingVideo from "../Media/landingpage.mp4";
 import styled from "styled-components";
 import Typist from "react-typist";
+import Particles from "react-particles-js";
 
 const Welcome = () => {
   return (
     <>
-      <Video autoPlay loop muted>
-        <source src={landingVideo} type="video/mp4" />
-      </Video>
-
-      <Typist cursor={{ hideWhenDone: true }}>
-        Hello, my name is Christian Bosse.
-        <Typist.Delay ms={1000} />
-        <br />
-        Welcome to my amaz
-        <Typist.Delay ms={0} />
-        nig
-        <Typist.Backspace count={3} delay={800} />
-        <Typist.Delay ms={600} />
-        ing Website
-        <Typist.Delay ms={1000} />
-        <br />
-      </Typist>
-
-      <Typist className="TypistExample-message" cursor={{ hideWhenDone: true }}>
-        * Easy to style
-        <Typist.Delay ms={1250} />
-        <br />
-        * Easy to customize
-        <Typist.Delay ms={1250} />
-        <br />
-        * Easy to use backp
-        <Typist.Delay ms={500} />
-        sace
-        <Typist.Backspace count={5} delay={1000} />
-        <Typist.Delay ms={750} />
-        space
-        <Typist.Delay ms={1250} />
-        <br />
-        <span>
-          * <span className="flash">docs</span>
-        </span>
-        <br />
-        {""}
-      </Typist>
+      {/* <Video autoPlay loop muted>
+        <source src={landingVideo} type="video/mp4" /> */}
+      <Particles />
+      <WelcomeText>
+        <Typist cursor={{ hideWhenDone: true }}>
+          Hello, my name is Christian Bosse.
+          <Typist.Delay ms={1000} />
+          <br />
+          Welcome to my amaz
+          <Typist.Delay ms={0} />
+          nig
+          <Typist.Backspace count={3} delay={800} />
+          <Typist.Delay ms={600} />
+          ing Website!
+          <Typist.Delay ms={1000} />
+          <br />
+          Im a Full-Stack Web Developer and love using
+          <Typist.Delay ms={500} />
+          <br />
+          React, CSS, Style-Components, Node.js, etc,
+          <Typist.Delay ms={500} />
+          <br />
+          to make my website come to live.
+          <Typist.Delay ms={1250} />
+          <br />
+          My Favorite hobby's are gaming and admiring nature.
+          <Typist.Delay ms={1250} />
+          <br />
+          You can check my <span className="flash">project</span> and{" "}
+          <span className="flash">contact</span> info below.
+        </Typist>
+      </WelcomeText>
+      {/* </Video> */}
     </>
   );
 };
@@ -54,6 +49,14 @@ const Video = styled.video`
   max-height: 100vh;
   object-fit: cover;
   filter: brightness(0.5);
+  z-index: 0;
+`;
+
+const WelcomeText = styled.div`
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default Welcome;
